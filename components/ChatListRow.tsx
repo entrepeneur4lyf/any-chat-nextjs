fe30c5af0fcd92819c3778a7995e379132ff2c02
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-11-22 21:05:22 +0530

    Update timestamp format in ChatListRow component

--- components/ChatListRow.tsx
+++ components/ChatListRow.tsx
@@ -55,7 +55,7 @@ const ChatListRow: React.FC<ChatListRowProps> = ({ chatId }) => {
         <div className="text-xs text-gray-400 text-right">
           <p className="mb-auto">
             {message
-              ? new Date(message.timestamp).toLocaleDateString()
+              ? new Date(message.timestamp).toLocaleTimeString()
               : "No messages yet"}
           </p>
           <p className="">chat #{prettyUUID()}</p>
