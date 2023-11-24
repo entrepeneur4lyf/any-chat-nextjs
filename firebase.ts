4c54517ab081f60157146e02a2f38cbfe78498b9
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-11-24 12:06:55 +0530

    Update Firebase configuration

--- firebase.ts
+++ firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAfqZJxs2UbtZCspZuGBWPr_AaTuhgsrRI",
-  authDomain: "any-chat-47898.firebaseapp.com",
-  projectId: "any-chat-47898",
-  storageBucket: "any-chat-47898.appspot.com",
-  messagingSenderId: "2252675046",
-  appId: "1:2252675046:web:139e1e777d4e26cc2be827",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
